1c10d4dd976a019327c479dd78fd737cb3f292db
Your Name <you@example.com>
2019-04-24 21:14:49 +0800

    update

--- source/preprocess.py
+++ source/preprocess.py
@@ -1,46 +1,33 @@
 import csv 
 import utils
 import random
-file_name = 'data/quora/train.csv'
-trainpath = 'data/train.txt'
-testpath = 'data/test.txt'
-referpath = 'data/refer.txt'
-train_object = open(trainpath, 'w')
-test_object = open(testpath, 'w')
-refer_object = open(referpath, 'w')
-testnum = 0
-with open(file_name) as f:
-    csv_reader = csv.reader(f, delimiter=',')
-    title = True
-    for row in csv_reader:
-        
-        ques1 = utils.clarify(row[3]).lower()
-        ques2 = utils.clarify(row[4]).lower()
 
-        if row[5]== '1' and testnum<1000 and len(ques1.split())<15 and random.random()>0.5:
-            test_object.write(ques1)
-            test_object.write('\n')
-            refer_object.write(ques2)
-            refer_object.write('\n')
-            testnum+=1
-        else:
+def readttest():
+    trainpath = 'data/train.txt'
+    train_object = open(trainpath, 'w')
+
+    file_name = 'data/quora-bak/test.csv'
+    with open(file_name) as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
+            ques1 = utils.clarify(row[1])
+            ques2 = utils.clarify(row[2])
             train_object.write(ques1)
             train_object.write('\n')
             train_object.write(ques2)
             train_object.write('\n')
-test_object.close( )
-refer_object.close( )
+    train_object.close( )
 
-# file_name = 'data/quora/test.csv'
-# with open(file_name) as f:
-#     csv_reader = csv.reader(f, delimiter=',')
-#     for row in csv_reader:
-#         ques1 = utils.clarify(row[3])
-#         ques2 = utils.clarify(row[4])
-# 
-#         train_object.write(ques1)
-#         train_object.write('\n')
-#         train_object.write(ques2)
-#         train_object.write('\n')
-# 
-train_object.close( )
+def reproducefile(filename):
+    trainpath = 'data/temp.txt'
+    train_object = open(trainpath, 'w')
+    with open(filename) as f:
+        for line in f:
+            s = utils.clarify(line)
+            train_object.write(s)
+    train_object.close( )
+
+if __name__ == "__main__":
+    # filename = 'data/tok.lower/quora_test_par.tok.lower'
+    # reproducefile(filename)
+    readttest()
